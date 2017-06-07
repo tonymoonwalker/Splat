11c14592a70cd8be6a3f718cc1b6123c88a22051
tonymoonwalker <tonymoonwalker@users.noreply.github.com>
2017-06-07 18:41:44 +0300

    updates

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,12 @@
-var $productName = $('.product-name');
+var $productName = $('.product_checkbox');
+$productName.filter(':checked').next().removeClass('product-name-hidden').addClass('product-name-visible-after');
+$productName.filter(':checked').prev().removeClass('product-name-hidden').addClass('product-name-visible-before');
 
 $productName.click(function () {
     $productName.filter(':checked').not(this).removeAttr('checked');
+    $(this).removeClass('product-name-visible-after');
+    $(this).removeClass('product-name-visible-before');
+    $(this).addClass('product-name-hidden');
+    $(this).next().removeClass('product-name-hidden').addClass('product-name-visible-after');
+    $(this).prev().removeClass('product-name-hidden').addClass('product-name-visible-before');
 });
