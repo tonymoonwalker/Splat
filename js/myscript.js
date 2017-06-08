34a45f65e6a0d00964acbcc26d1221d6bdbce57b
tonymoonwalker <tonymoonwalker@gmail.com>
2017-06-08 09:28:18 +0300

    update

--- js/myscript.js
+++ js/myscript.js
@@ -1,12 +1,15 @@
 var $productName = $('.product_checkbox');
+
 $productName.filter(':checked').next().removeClass('product-name-hidden').addClass('product-name-visible-after');
 $productName.filter(':checked').prev().removeClass('product-name-hidden').addClass('product-name-visible-before');
 
 $productName.click(function () {
     $productName.filter(':checked').not(this).removeAttr('checked');
-    $(this).removeClass('product-name-visible-after');
-    $(this).removeClass('product-name-visible-before');
-    $(this).addClass('product-name-hidden');
-    $(this).next().removeClass('product-name-hidden').addClass('product-name-visible-after');
-    $(this).prev().removeClass('product-name-hidden').addClass('product-name-visible-before');
+    var $productCounter = $('.product_checkbox').length;
+    alert($productCounter);
+    var $productIndex = indexOf($productName);
+    alert($productIndex);
+    //    $productName.removeClass('product-name-visible-after product-name-visible-before').addClass('product-name-hidden');
+    //    if $(this).prev().addClass('product-name-visible-before');
+    //    $(this).not(last).next().addClass('product-name-visible-after');
 });
